17d8d4ab52eb7cede457c562d93b59f75b647a4d
William.Potter <rainstorm.exp@gmail.com>
2024-02-06 18:15:48 +0900

    ready

--- app/ui/customers/table.tsx
+++ app/ui/customers/table.tsx
@@ -1,10 +1,10 @@
 import Image from 'next/image';
-import { lusitana } from '../app/ui/fonts';
-import Search from '../app/ui/search';
+import { lusitana } from '../fonts';
+import Search from '../search';
 import {
   CustomersTableType,
   FormattedCustomersTable,
-} from '../app/lib/definitions';
+} from '../../lib/definitions'
 
 export default async function CustomersTable({
   customers,
